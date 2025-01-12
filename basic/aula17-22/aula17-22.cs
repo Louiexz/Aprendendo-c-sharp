7136e54eddf34142cc1030c5368094e80078708c
Luiz Augusto <127119543+Louiexz@users.noreply.github.com>
2025-01-12 13:42:31 +0000

    Feat: Aprendendo Loops

---
+++ basic/aula17-22/aula17-22.cs
@@ -0,0 +1,31 @@
+using System;
+
+class AulasDezesseteVinteDois {
+  static void Main() {
+    int[] n = new int[3]{19, 20, 21};
+    
+    inicio:
+    
+    try{
+        for(int i=0; i < n.Length; i++){
+            Console.WriteLine("Por favor. Digite um número? ");
+            n[i] = int.Parse(Console.ReadLine());
+        }
+    } catch (Exception) {
+        Console.WriteLine("Por favor. Digite apenas números inteiros.");
+        goto inicio;
+    }
+    Console.WriteLine();
+    
+    foreach(int num in n){
+        Console.WriteLine("Números: {0}",num);
+    }
+    
+    Console.WriteLine("\nDeseja continuar? ");
+    string choose = Console.ReadLine();
+    
+    while (choose == "s" | choose == "sim"){
+        goto inicio;
+    }
+  }
+}
